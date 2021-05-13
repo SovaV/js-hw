e578feefc3c42ed0b17570c15d80d8c27f2a1c50
SovaV <sova.volodymyr95@gmail.com>
2021-05-13 11:32:08 +0300

    main

--- js/31.js
+++
@@ -1,8 +0,0 @@
-const courseTopic = "JavaScript essentials";
-// Change code below this line
-
-const courseTopicLength = courseTopic.length;
-const firstElement = courseTopic[0];
-const lastElement = courseTopic[courseTopic.length - 1];
-
-// Change code above this line
