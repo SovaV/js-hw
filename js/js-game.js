0429e4f1f06c0bcbbe322ffebc25cda0ce3b6822
SovaV <sova.volodymyr95@gmail.com>
2021-05-13 11:31:03 +0300

    гра чисел

---
+++ js/js-game.js
@@ -0,0 +1,19 @@
+"use strict";
+const secretNumber = Math.round(Math.random() * 100 + 1);
+// console.log(secretNumber);
+
+let userNumber;
+do {
+  const userAnswer = prompt("Enter a number");
+  userNumber = Number.parseInt(userAnswer);
+  console.log("userNumber:", userNumber);
+  if (secretNumber === userNumber) {
+    console.log("You won!");
+  } else if (secretNumber > userNumber) {
+    console.log("x >", userNumber);
+  } else if (secretNumber < userNumber) {
+    console.log("x <", userNumber);
+  } else {
+    console.log("exit");
+  }
+} while (secretNumber !== userNumber && !isNaN(userNumber));
