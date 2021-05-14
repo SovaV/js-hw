783d091b88ab85e2c44bca4261b2c873294df912
SovaV <sova.volodymyr95@gmail.com>
2021-05-14 17:02:42 +0300

    1

---
+++ js/gj.js
@@ -0,0 +1,14 @@
+function calculateEngravingPrice(message, pricePerWord) {
+  const words = "message name, lol";
+  console.log(words);
+
+  const x = words.split(" ").length;
+  console.log(x * 5);
+}
+// function add(mess, c) {
+//   console.log(`Addition result equals ${mess + c}`);
+// }
+
+// add(2, 10);
+// add(10, 30);
+// add(5, 15);
--- js/hw210.js
+++ js/hw210.js
@@ -1,6 +1,4 @@
 function splitMessage(message, delimeter) {
-  const words = message.split(delimeter);
-  // Change code below this line
-  // Change code above this line
+  let words = message.split(delimeter);
   return words;
 }
