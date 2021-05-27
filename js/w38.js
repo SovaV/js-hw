d124c9107af68c147b5bceb64173da8f77e57e4f
SovaV <sova.volodymyr95@gmail.com>
2021-05-27 16:39:15 +0300

    1

---
+++ js/w38.js
@@ -0,0 +1,27 @@
+const atTheOldToad = {
+  potions: ["Speed potion", "Dragon breath", "Stone skin"],
+  addPotion(potionName) {
+    this.potions.push(potionName);
+
+    return;
+  },
+};
+
+/*
+Задача. Добавляем новое зелье
+Задание
+Дополни метод addPotion(potionName) так, чтобы он добавлял зелье potionName в конец массива зелий в свойстве potions.
+
+Тесты
+Объявлена переменная atTheOldToad
+
+Значение переменной atTheOldToad это объект
+
+Значение свойства atTheOldToad.potions это массив ['Speed potion', 'Dragon breath', 'Stone skin']
+
+Значение свойства atTheOldToad.addPotion это метод объекта
+
+После первого вызова метода atTheOldToad.addPotion('Invisible'), в свойстве potions будет массив ['Speed potion', 'Dragon breath', 'Stone skin', 'Invisible']
+
+После второго вызова метода atTheOldToad.addPotion('Зелье силы'), в свойстве potions будет массив ['Speed potion', 'Dragon breath', 'Stone skin', 'Invisible', 'Power potion']
+*/
