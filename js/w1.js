a3e1e372463ff59286537703fd2204740c0d190e
SovaV <sova.volodymyr95@gmail.com>
2021-06-02 16:33:15 +0300

    1

---
+++ js/w1.js
@@ -0,0 +1,12 @@
+const numbers = [1, 2, 3];
+
+const doubledNumbers = numbers.map((num) => num > 2);
+console.log(doubledNumbers); // [2, 4, 6]
+
+// const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// console.log(numbers.filter((num) => num * 5)); // [6, 7, 8, 9]
+
+// console.log(numbers.filter((num) => num < 5)); // [0, 1, 2, 3, 4]
+
+// console.log(numbers.filter((num) => num === 5)); // [5]
