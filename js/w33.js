16f46a71387513bce26d0a2da80cfb9f664dbc4c
SovaV <sova.volodymyr95@gmail.com>
2021-06-07 12:24:11 +0300

    Update w33.js

--- js/w33.js
+++ js/w33.js
@@ -1,16 +1,84 @@
-const tweets = [
-  { id: "000", likes: 5, tags: ["js", "nodejs"] },
-  { id: "001", likes: 2, tags: ["html", "css"] },
-  { id: "002", likes: 17, tags: ["html", "js", "nodejs"] },
-  { id: "003", likes: 8, tags: ["css", "react"] },
-  { id: "004", likes: 0, tags: ["js", "nodejs", "react"] },
-];
+const getTotalFriendCount = users => {
+   const friends = users.reduce((total, user) => total + user.friends.length, 0)
+   return friends;
+};
 
-const getTags = (tweets) =>
-  tweets.reduce((allTags, tweet) => {
-    allTags.push(...tweet.tags);
+/*
+Задача. Общее количество друзей
+Этот массив объектов мы будем передавать в параметр users при вызове функции из задания.
 
-    return allTags;
-  }, []);
+[
+  {
+    name: 'Moore Hensley',
+    email: 'moorehensley@indexia.com',
+    eyeColor: 'blue',
+    friends: ['Sharron Pace'],
+    isActive: false,
+    balance: 2811,
+    gender: 'male'
+  },
+  {
+    name: 'Sharlene Bush',
+    email: 'sharlenebush@tubesys.com',
+    eyeColor: 'blue',
+    friends: ['Briana Decker', 'Sharron Pace'],
+    isActive: true,
+    balance: 3821,
+    gender: 'female'
+  },
+  {
+    name: 'Ross Vazquez',
+    email: 'rossvazquez@xinware.com',
+    eyeColor: 'green',
+    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+    isActive: false,
+    balance: 3793,
+    gender: 'male'
+  },
+  {
+    name: 'Elma Head',
+    email: 'elmahead@omatom.com',
+    eyeColor: 'green',
+    friends: ['Goldie Gentry', 'Aisha Tran'],
+    isActive: true,
+    balance: 2278,
+    gender: 'female'
+  },
+  {
+    name: 'Carey Barr',
+    email: 'careybarr@nurali.com',
+    eyeColor: 'blue',
+    friends: ['Jordan Sampson', 'Eddie Strong'],
+    isActive: true,
+    balance: 3951,
+    gender: 'male'
+  },
+  {
+    name: 'Blackburn Dotson',
+    email: 'blackburndotson@furnigeer.com',
+    eyeColor: 'brown',
+    friends: ['Jacklyn Lucas', 'Linda Chapman'],
+    isActive: false,
+    balance: 1498,
+    gender: 'male'
+  },
+  {
+    name: 'Sheree Anthony',
+    email: 'shereeanthony@kog.com',
+    eyeColor: 'brown',
+    friends: ['Goldie Gentry', 'Briana Decker'],
+    isActive: true,
+    balance: 2764,
+    gender: 'female'
+  }
+]
+Задание
+Дополни функцию getTotalFriendCount(users) так, чтобы она считала и возвращала общее количество друзей (свойство friends) всех пользователей из массива users.
 
-console.log(getTags(tweets));
+Тесты
+Объявлена переменная getTotalFriendCount.
+Переменной getTotalFriendCount присвоена стрелочная функция с параметром (users).
+Для перебора параметра users используется метод reduce().
+Вызов функции с указанным массивом пользователей возвращает число 14
+Вызов функции со случайными, но валидными аргументами, возвращает правильное значение.
+*/
